1c6afa7bca3b68612a2ac4200e49dbc07e801480
jlcastro1877 <jlcastro1877@gmail.com>
2024-04-04 20:41:51 -0600

    implemented variables and listen to modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,60 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+const projectFormEl = $('#project-form');
+const projectNameInputEl = $('#project-name-input');
 const projectDateInputEl = $('#taskDueDate');
+const projectTypeInputEl = $("#message-text");
+
+
+// ? Add event listener to the form element, listen for a submit event, and call the `handleProjectFormSubmit` function.
+projectFormEl.on('submit', handleProjectFormSubmit);
+
+// ? Adds a project to local storage and prints the project data
+function handleProjectFormSubmit(event) {
+  event.preventDefault();
+  console.log('Entrou aqui');
+
+  // ? Read user input from the form
+  const projectName = projectNameInputEl.val().trim();
+  const projectType = projectTypeInputEl.val(); // don't need to trim select input
+  const projectDate = projectDateInputEl.val(); // yyyy-mm-dd format
+ 
+
+  const newProject = {
+  // ? Here we use a Web API called `crypto` to generate a random id for our project. This is a unique identifier that we can use to find the project in the array. `crypto` is a built-in module that we can use in the browser and Nodejs.    id: crypto.randomUUID(),
+    name: projectName,
+    type: projectType,
+    dueDate: projectDate,
+    status: 'to-do',
+  };
+}
+
+$('#taskDueDate').datepicker({
+    changeMonth: true,
+    changeYear: true,
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -38,7 +91,3 @@ $(document).ready(function () {
 
 });
 
-$('#taskDueDate').datepicker({
-    changeMonth: true,
-    changeYear: true,
-  });
